b5dab3d92e713353f80dae5ab32c03aaa54619d4
afuma <etienne.42paris@gmail.com>
2024-09-20 12:31:43 +0200

    first tests docker-compose

--- inception/srcs/requirements/wordpress/tools/config.sh
+++ inception/srcs/requirements/wordpress/tools/config.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # Attendre 10 secondes pour s'assurer que les services nécessaires (comme la base de données) sont en ligne
-sleep 10
+sleep 5
 
+cd /var/www/wordpress #peut etre pas necessaire
+FILE=/var/www/wordpress/wp-config.php
 # Créer wp-config.php si nécessaire
-if [ ! -f /var/www/wordpress/wp-config.php ]; then
+if [ ! -f "$FILE" ]; then
   wp config create --allow-root \
                    --dbname="$SQL_DATABASE" \
                    --dbuser="$SQL_USER" \
@@ -18,15 +20,16 @@ fi
 
 # Lancer la configuration WordPress
 wp core install --allow-root \
-                --url="http://example.com" \
-                --title="Mon Site" \
-                --admin_user="admin" \
-                --admin_password="password" \
-                --admin_email="admin@example.com" \
-                --path='/var/www/wordpress'
+                --url=https://${DOMAIN_NAME} \
+                --title=${SITE_TITLE} \
+                --admin_user=${ADMIN_USER} \
+                --admin_password=${ADMIN_PASSWORD} \
+                --admin_email=${ADMIN_EMAIL}
 
 # Créer un deuxième utilisateur WordPress
-wp user create seconduser user@example.com --role=editor --user_pass=password --allow-root --path='/var/www/wordpress'
+wp user create --allow-root ${USER1_LOGIN} ${USER1_MAIL} \
+	--role=author \
+	--user_pass=${USER1_PASS}
 
 # Lancer PHP-FPM
 /usr/sbin/php-fpm7.3 -F
